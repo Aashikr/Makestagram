9df9c184aaf488d45e7a3dce02f55b17d1dbb12f
sakadalim <sakadalim@gmail.com>
2017-07-25 16:35:52 -0400

    Added App Style Sheet
    Add OpenSans Font, and color scheme for the app

---
+++ Makestagram/Makestagram/Extensions/UIFont+Utility.swift
@@ -0,0 +1,15 @@
+//
+//  UIFont+Utility.swift
+//  Makestagram
+//
+//  Created by Sakada Lim on 7/25/17.
+//  Copyright © 2017 Sakada Lim. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    static var mgRegular: UIFont {
+        return UIFont(name: "OpenSans", size: 16)!
+    }
+}
